91e543b5a6fe3794e947da23cbf1f05c4d27e79e
Kristian Jay Tadeo <Kristianjaytadeo11@gmail.com>
2024-10-19 00:01:11 +0800

    make some changes "Flexible grids and layouts"

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -8,6 +8,7 @@ use App\Models\Project;
 use Filament\Forms;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Section;
@@ -17,6 +18,7 @@ use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\MaxWidth;
 use Filament\Support\Markdown;
 use Filament\Tables;
@@ -47,6 +49,7 @@ class ProjectResource extends Resource
                     TextInput::make('name')
                         ->label('Name')
                         ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Enter the name of the project.')
+                        ->columnSpan(8)
                         ->required(),
                     
                         Select::make('status')
@@ -57,45 +60,59 @@ class ProjectResource extends Resource
                             'completed' => 'Completed',
                         ])
                         ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Select the current status of the project.')
+                        ->columnSpan(4)
                         ->default('not-started')
                         ->required(),
-                    DatePicker::make('start_date')
-                        ->label('Start Date')
-                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Select the start date of the project.')
-                        ->required(),
-                    DatePicker::make('end_date')
-                        ->label('End Date')
-                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Select the end date of the project.')
-                        ->required(),
+            
                     Textarea::make('description')
                         ->label('Description')
                         ->columnSpanFull()
                         ->rows(10)
                         ->cols(20)
                         ->autosize()
+                        ->columnSpan(12)
                         ->minLength(2)
                         ->maxLength(1024)
                         ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Provide a detailed description of the project. The description should be between 2 and 1024 characters.')
                         ->required(),
-            ]),
 
+                        DatePicker::make('start_date')
+                        ->label('Start Date')
+                        ->columnSpan(6)
+                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Select the start date of the project.')
+                        ->required(),
+                        
+                        DatePicker::make('end_date')
+                        ->label('End Date')
+                        ->columnSpan(6)
+                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Select the end date of the project.')
+                        ->required(),
+            ])->columns(12)->columnSpanFull(),
 
             Fieldset::make('Metadata')->schema([
-                Placeholder::make('start_date')
-                    ->content(fn (?Project $record): string => optional($record)->start_date?->toFormattedDateString() ?? 'N/A'),
-                    
-                    Placeholder::make('end_date')
-                    ->content(fn (?Project $record): string => optional($record)->start_date?->toFormattedDateString() ?? 'N/A'),
-                        
-                    Placeholder::make('created_at')
-                    ->content(fn (?Project $record): string => optional($record)->created_at?->toFormattedDateString() ?? 'N/A'),
-                    
-                    Placeholder::make('updated_at')
-                    ->content(fn (?Project $record): string => optional($record)->updated_at?->toFormattedDateString() ?? 'N/A'),
-                    
-            ])->grow(false)->columnSpanFull(),
+                                Placeholder::make('start_date')
+                                ->content(fn (?Project $record): string => optional($record)->start_date?->toFormattedDateString() ?? 'N/A'),
+                                
+                                Placeholder::make('end_date')
+                                ->content(fn (?Project $record): string => optional($record)->start_date?->toFormattedDateString() ?? 'N/A'),
 
-        ])->from('sm')->columnSpanFull(),
+                                Placeholder::make('created_at')
+                                ->content(fn (?Project $record): string => optional($record)->created_at?->toFormattedDateString() ?? 'N/A'),
+
+                                Placeholder::make('updated_at')
+                                ->content(fn (?Project $record): string => optional($record)->updated_at?->toFormattedDateString() ?? 'N/A'),
+                            ])->grow(false)->columnSpan(12),
+
+        ])->from('sm')->columns(
+            [
+                'sm' => 3,
+                'md' => 6,
+                'lg' => 8,
+                '2xl' => 12,
+            ]
+
+
+        )->columnSpanFull()
 
             ]);
     }
@@ -113,7 +130,7 @@ class ProjectResource extends Resource
                 TextColumn::make('description')
                     ->searchable()
                     ->sortable()
-                    ->limit(20)
+                    ->limit(40)
                     ->label('Description'),
                 TextColumn::make('status')
                     ->searchable()
